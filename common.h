92f545049f8c168f3af6bbb568eb429505158cc6
Mohamed Rezq <93948617+mohamed22311@users.noreply.github.com>
2024-05-21 00:02:58 +0300

    Update common.h

--- common.h
+++ common.h
@@ -2,12 +2,17 @@
 #define _INCLUDE_COMMON_H_
 
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include <unistd.h>
 
 // from `man shm_open`
+
 #include <sys/mman.h>
+
 #include <sys/stat.h>        /* For mode constants */
+
 #include <fcntl.h>           /* For O_* constants */
 
 #include <semaphore.h>
